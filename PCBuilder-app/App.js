6a002aeabb9e4495879d7fbee8f6d2dae39a8dfb
Ali khalid <alialmaghamsi7@gmail.com>
2024-01-12 03:39:51 +0300

    Good code

--- PCBuilder-app/App.js
+++ PCBuilder-app/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ChatScreen from './app/Pages/ChatScreen';
+import HomeScreen from './app/Pages/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeNavigation from './app/Navigation/HomeScreenNavigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatScreen/>
+      <NavigationContainer>
+      <HomeNavigation/>
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,5 +19,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    
   },
 });
